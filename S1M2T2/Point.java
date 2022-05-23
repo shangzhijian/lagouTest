2c4e6caaf37f5c2d3aa2995245a7c4585d7eeb1f
shangzhijian <1037390087@qq.com>
2022-05-23 23:10:58 +0800

    Point类实现重载

--- S1M2T2/Point.java
+++ S1M2T2/Point.java
@@ -1,5 +1,8 @@
 /*
-	编程实现Point类
+	编程实现Point类添加重载的成员方法：
+		up()一实现纵坐标减1的功能。
+		up(int dy)一实现纵坐标减去参数指定数值的功能。
+	测试重载方法的调用规则
  */
 
 public class Point{
@@ -20,13 +23,29 @@ public class Point{
 		System.out.println("横坐标是：" + x + "纵坐标是：" + y);
 	}
 
+	void up(){
+		y--;
+	}
+
+	void up(int dy){
+		y -= dy;
+	}
+
+
+
 
 	public static void main(String[] args){
 		Point p = new Point();
 
 		p.show();
 
-		Point p1= new Point(1,2);
+		Point p1= new Point(4,5);
+		p1.show();
+
+		p1.up();
+		p1.show();
+
+		p1.up(3);
 		p1.show();
 	}
 }
