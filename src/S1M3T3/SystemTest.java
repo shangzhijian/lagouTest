871d510895d02e5463b5492e8d54f77f0747c634
shangzhijian <1037390087@qq.com>
2022-07-21 16:40:34 +0800

    java8之前日期类练习，包括System, Date, SimpleDateFormat, Calendar

---
+++ src/S1M3T3/SystemTest.java
@@ -0,0 +1,12 @@
+
+
+public class SystemTest {
+
+	public static void main(String[] args) {
+		while(true) {
+		long t = System.currentTimeMillis();
+		System.out.println("当前距离1970年1月1日0时0分0秒已经过去了" + t + "毫秒了！");
+		}
+		
+	}
+}
